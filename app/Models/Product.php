ca4aec1152903e96345016fd7f399db4f3cc32c1
Francesco Limpias <francescolimpias@gmail.com>
2023-02-15 22:45:53 +0100

    Added models guards and relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $guarded = ["id"];
+
+    /**
+     * The categories that belong to the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    /**
+     * Get the type that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+    
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+    protected $guarded = ["id"];
+
+    /**
+     * Get all of the products for the Type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
