38146560f7407e6e3bef4d413cc03933ba4f6b9a
Francesco Limpias <francescolimpias@gmail.com>
2023-02-16 11:07:00 +0100

    Created base routes and views

---
+++ resources/views/product/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('main')
+    <h2>
+        product.home
+    </h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/products', function () {
+    return view('product.home');
+});
+
+Route::get('/products/create', function () {
+    return view('product.create');
 });
