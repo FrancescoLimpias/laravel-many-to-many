ca97224c3601be870e808397d74cd5047b7f4e2a
Francesco Limpias <francescolimpias@gmail.com>
2023-02-16 10:18:27 +0100

    Added seeding logics

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,12 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::factory()->count(50)->make()->each(function(Product $product){
+
+            $product->type()->associate(Type::inRandomOrder()->first());
+            $product->save();
+            $product->categories()->attach(Category::inRandomOrder()->limit(rand(1,5))->get());
+
+        });
     }
 }
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Type::factory()->count(10)->create();
     }
 }
