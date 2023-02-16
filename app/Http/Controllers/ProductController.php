1e4eb5b84e277db6eff65b1a180f055800efe8b7
Francesco Limpias <francescolimpias@gmail.com>
2023-02-16 11:55:07 +0100

    Added Home view Main

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -24,7 +25,8 @@ class ProductController extends Controller
      */
     public function indexByCategory()
     {
-        return view('home');
+        $categories = Category::all();
+        return view('home', compact("categories"));
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,25 @@
     <h2>
         home
     </h2>
+
+    <ul>
+        @foreach ($categories as $category)
+            <li class="category-{{ $category['code'] }}">
+                <p>
+                <h3>
+                    {{ $category['name'] }}
+                </h3>
+
+                <ul>
+                    @foreach ($category->products as $product)
+                        <li class="product-{{ $product['code'] }}">
+                            {{ $product['name'] }}
+                            {{ $product->type->digital ? "(digital)" : ""}}
+                        </li>
+                    @endforeach
+                </ul>
+                </p>
+            </li>
+        @endforeach
+    </ul>
 @endsection
